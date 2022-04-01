0f0f4bc7fb45b9131d81c06192c62b4d6149e72b
steveyxz <stevenzhu2009@outlook.com>
2022-04-01 17:44:30 +1100

    Initial commit

---
+++ src/main/java/me/magikus/Magikus.java
@@ -0,0 +1,14 @@
+package me.magikus;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Magikus extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+}
